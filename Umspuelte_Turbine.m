512b2a3e7554188f10a0fd6d9529375675fd7928
Jumper Schlumpf <schlumpfjumper@gmail.com>
2023-05-19 15:42:56 +0200

    Umströmte Fläche eine Gondel fertig

--- Umspuelte_Turbine.m
+++ Umspuelte_Turbine.m
@@ -1,32 +1,42 @@
 %%
 %%   Quelle: E. Torenbeek: "Synthesis of Subsonic Airplane Design", Appendix B S446-449
-%%Umströmte Fläche Turbine
+%%Umströmte Fläche für EEEEEEIIIIINNNNNEEEEEEE Gondel
+
+clc
+clear all
 %%
+feat=3.28084;      %%Umrechnungsfactor
+ln=4*feat;        %%länge fan cowling
+lh=2.3*feat;      %%länge zur größten Stelle an der urbine
+Dn=3.3*feat;      %%größter Durchmesser Engine
+Dh=3*feat;        %%Durchmesser Eingang Engine
+Gesamtlaenge = 4.49*feat;
+beta_c = ln/Gesamtlaenge;   %%Gesamtlänge des Triebwerkes
 
-ln=4;        %%länge fan cowling
-lh=2.3;      %%länge zur größten Stelle an der urbine
-Dn=3.3;      %%größter Durchmesser Engine
-Dh=3;        %%Durchmesser Eingang Engine
-Gesamtlaenge= 4,49;
-beta_c=ln/Gesamtlaenge;   %%Gesamtlänge des Triebwerkes
-
-A_cowling=ln*Dn*(2+0.35*beta*(Dh/Dn)+1.15*(1-beta))
+A_cowling = ln*Dn*(2+0.35*beta_c*(Dh/Dn)+1.15*(1-beta_c));
 
 %%
 %%Gas Generator section
 %%
 
-lg=0.3;     %%Länge section
-Dg=1.3;     %%größter Durchmesser section
-Deg=0.9;    %%KLeinster Durchmesser section
-beta_g=lg/Gesamtlaenge;
+lg=0.3*feat;     %%Länge section
+Dg=1.3*feat;     %%größter Durchmesser section
+Deg=0.9*feat;    %%KLeinster Durchmesser section
 
-A_generator=pi*lg*Dg*(1-((1/3)*(1-(Deg/Dg)))*1-0.18*((Dg/lg)^(5/3)))
+A_generator = pi*lg*Dg*(1-((1/3)*(1-(Deg/Dg)))*1-0.18*((Dg/lg)^(5/3)));
+
+
 
 %%
 %%Plug
 %%
-lp=0.19;    %%Länge section
-Dp=0.5;     %%Anfangs Durchmesser
+lp=0.19*feat;    %%Länge section
+Dp=0.5*feat;     %%Anfangs Durchmesser
 
-A_plug=0.7*pi*lp*Dp
+A_plug=0.7*pi*lp*Dp;
+
+
+%%
+%%Alles zsm
+
+A_Turbine=(A_plug+A_cowling+A_plug)/(feat^2)

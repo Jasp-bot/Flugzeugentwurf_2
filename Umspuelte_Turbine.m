3820723eaa89eab042914fc6541cf8386c80c431
Jumper Schlumpf <schlumpfjumper@gmail.com>
2023-05-19 16:45:43 +0200

    Gewicht der pylone und Triebwerksummantelungen

--- Umspuelte_Turbine.m
+++ Umspuelte_Turbine.m
@@ -4,8 +4,10 @@
 
 clc
 clear all
+load Projekt_specs.mat;
+
 %%
-feat=3.28084;      %%Umrechnungsfactor
+feat=3.28084;     %%Umrechnungsfactor
 ln=4*feat;        %%länge fan cowling
 lh=2.3*feat;      %%länge zur größten Stelle an der urbine
 Dn=3.3*feat;      %%größter Durchmesser Engine
@@ -39,4 +41,21 @@ A_plug=0.7*pi*lp*Dp;
 %%
 %%Alles zsm
 
-A_Turbine=(A_plug+A_cowling+A_plug)/(feat^2)
+A_Turbine=(A_plug+A_cowling+A_plug);
+
+%%
+%%Gewicht berechnen nach:
+%%Commercinal airplane design principals
+%%  Refined Weight and Balance Estimate  s.317
+
+W_e=2.20462*specs.m_TW;       %%Gewicht Triebwerk
+S_p=10.3*feat^2;              %%umspülte fläche des pilon
+n_ult=3.2;                    %%Danach soll itteriert werden sagt jasper
+
+
+W_n=35.45*2*((2.33*(1.1*W_e)*A_Turbine)/(10000))^(0.59);    %%Ausrechnen in pounds
+W_p=24.11*2*S_p^(0.381)*((1.46*n_ult*1.1*W_e*ln*Dn)/(10^6*cos(deg2rad(80)))^(0.952));
+
+Gewicht=W_n/2.20462
+Gewicht=W_p/2.20462
+

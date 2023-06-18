1f30eda35e50cc1b8b2c848c5f07122687ec48df
Maciej Janowski <mac.janowski@gmx.de>
2023-06-18 17:17:53 +0200

    Fahrwerk Fertig
    Braucht nur Schwerpunkt Positionen/Geschw./GTO/ Iteration über Sb und St. @kristof fragen wegen Durchmesser Reifen. Womöglich muss eine Variable dann verändert werden und leicht umgeschrieben werden :)

--- PS_X_Fahrwerk.m
+++ PS_X_Fahrwerk.m
@@ -1,70 +1,31 @@
 clear
-close
+close  
 clc
 
-G_to = 200000;
+G_to = 300000;
 l_HFW = 90;
 l_BFW = 20;
 delta_z = 10;
 l_BFW_max = 25;
 l_BFW_min = 18;
-d_reifen = 17;
+%d_reifen = 17;
 l_HFW_min = 20;
 load Projekt_specs.mat
 
+% Reifendatenbank
+tires = readtable('Reifendaten_angepasst_Flaeche.xlsx');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Bugfahrwerk
-%min stat. Belastung
-F_BFW_min = (G_to * ((l_BFW+l_HFW)-l_BFW_max))/(l_BFW + l_HFW);
+%% 0. Reifengeschwindigkeiten
 
-%Max stat. Belastung
-F_BFW_max = (G_to * ((l_BFW + l_HFW)-l_BFW_min))/(l_BFW + l_HFW);
-
-%Max dyn. Belastung
-F_BFW_dyn = F_BFW_max + ((10*(delta_z + 0.5 * d_reifen)*G_to) / (32.2 * (l_BFW + l_HFW)));
-
-% Muss zwischen 6% und 20% des Abfluggewichts liegen
-
-if ((F_BFW_min >= 0.06*G_to) && (F_BFW_max <= 0.2*G_to))
-    disp("Within Limits 1 ohne 7%");
-    % Wenn Lasten standhalten dann nochmal zusätzlicher Faktor von 7% und
-    % nochmal vergleichen
-    F_BFW_min = F_BFW_min + F_BFW_min * 0.07;
-    F_BFW_max = F_BFW_max + F_BFW_max * 0.07;
-    if ((F_BFW_min >= 0.06*G_to) && (F_BFW_max <= 0.2*G_to))
-        disp("Within Limits 2 mit 7%");
-    else
-        disp("Außerhalb Limits 2 mit 7%");
-    end
-else
-    disp("Außerhalb Limits 1 ohne 7%");
-end
-%weitere 25% können weggelassen werden weil Roskam/Torenbeek gemacht wurde
-%% Hauptfahrwerk
-n_FWB = 2; % Oder 3?
-
-F_HFW_max = (G_to *((l_BFW + l_HFW) - l_HFW_min)) / (n_FWB * (l_BFW + l_HFW));
-
-
-%% Entwurfslasten der Reifen    % PRO REIFEN !!
-S_Reserve = 0.25;
-S_FW = 1 + ((0.07 + S_Reserve)/100);
-
-n_reifen_BFW = 2;
-n_reifen_HFW = 12;
-
-F_reifen_BFW_max = (F_BFW_max/n_reifen_BFW) * S_FW;
-F_reifen_BFW_dyn = (F_BFW_dyn/n_reifen_BFW) * S_FW;
-
-F_reifen_HFW_max = (F_HFW_max/n_reifen_HFW) * S_FW;
-
-%% Reifengeschwindigkeiten max
-
-v_s_LDG = 80;
-v_s_TO = 100;
+v_s_LDG = 80; %-> Specs
+v_s_TO = 100; %-> Specs
 
 v_reifen_max_LDG = 1.3 * v_s_LDG;
 v_reifen_max_TO = 1.2 * v_s_TO;
+
+%Ist v_takeoff/Landing aus Spezifikation größer als diese Rechnung dann
+%diese verwenden
 
 if (v_reifen_max_LDG > v_reifen_max_TO)
     v_reifen_krit = v_reifen_max_LDG;
@@ -72,45 +33,48 @@ else
     v_reifen_krit = v_reifen_max_TO;
 end
 
-%% Reifendruck 
-% Bugfahrwerk High Pressure ab 160PSI/ Aber auch 220PSI
-% Hauptfahrwerk Low-Pressure
+v_reifen_krit = v_reifen_krit * 1.151078;
 
+%% 1. Hauptfahrwerk Belastung ermitteln
+S_Reserve = 0.25;
+S_FW = 1 + ((0.07 + S_Reserve)/100);
+
+n_FWB = 2; % Es handelt sich um Hauptfahrwerksbeine
+n_reifen_HFW = 12; % Anzahl Reifen insgesamt
+
+F_HFW_max = (G_to *((l_BFW + l_HFW) - l_HFW_min)) / (n_FWB * (l_BFW + l_HFW));
+
+F_reifen_HFW_max = (F_HFW_max/n_reifen_HFW) * S_FW;
+
+idx = (tires.RatedLoad_Lbs_ >= F_reifen_HFW_max & tires.RatedInflation_PSI_<=220 & tires.RatedSpeed_MPH_ >= v_reifen_krit);
+tires_edit_HFW = tires(idx,:);
+
+
+%% 2. LCN bestimmen -> beste Reifen wählen
 
 %% BEstimmung LCN 
 
-S_b =118; %Wheel Base von 1. bis 3. Rad
-S_t = 20; % Spurbreite / Track width
-tot_load = 9000; % Total Load on one undercarriage assembly
-L = 40; %Relative Steifigkeit in inch
+S_b = 50; %Wheel Base von 1. bis 3. Rad 
+S_t = 30; % Spurbreite / Track width        -> Variabel für Iteration
 
-% Reifendatenbank
-tires = readtable('Reifendaten_angepasst_Flaeche.xlsx');
+
+tot_load = F_HFW_max; % Total Load on one undercarriage assembly
+L = 40; %Relative Steifigkeit in inch
+n_rad = 6; % Sechs Räder pro Undercarriage ASSEMBLY
 
 % ESWL bestimmen
+
 % Erster Schritt Sb/L
 T1 = readmatrix('ReductionFactors1.csv');
-T3 = readmatrix("LCN_new.csv");
-T2 = readmatrix('reductionfactors2.csv');
-
-for i=1:length(T2(:,1))
-    for ii=1:length(T2(i,:))
-        if isnan(T2(i,ii))
-            T2(i,ii) = 0;
-        end
-    end
-
-end
-
 
 Sb_L = S_b / L; % in inch ?
 St_L = S_t / L; % in Inch ?
+Y = roundtowardvec(Sb_L,[25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 110 120 130 140 150 160 170]);
 
-rounding = (round(St_L*2)/2);
 
 % Finde passende Spalte
-[~,j]=find(T1(1,:)==(St_L*100));
-
+figure(1)
+[~,j]=find(T1(1,:)==(Y));
 plot(T1(:,j),T1(:,j+1));
 hold on
 z(:,1) = T1(:,j);
@@ -120,51 +84,63 @@ z(1,2) = 0;
 z(2,1) = 0;
 z(2,2) = 0;
 
-
-
 %%PLotten zum Prüfen
-punkt_x = [Sb_L,Sb_L];
-punkt_y = [0,50];
-punkt = [Sb_L Sb_L;0 50];
-plot(punkt_x, punkt_y);
-
+ punkt_x = [Sb_L,Sb_L];
+ punkt_y = [0,50];
+ punkt = [Sb_L Sb_L;0 50];
+ plot(punkt_x, punkt_y);
+ hold off
 
 % interx Schnittpunkt finden
 
 P = InterX(z.',punkt);
 
 
-% Zweiter Schritt Reduction Factor
 
+% Zweiter Schritt Reduction Factor und Contact Area
+T2 = readmatrix('reductionfactors2.csv');
+T2(:,(1:46)) = 0;
 
-%Anzahl Räder an  einer Assembly
-n_rad = 6;
+T3 = readmatrix("LCN_new.csv");
 
 figure(3);
-for ii=1:2:19
-loglog(T3(:,ii),T3(:,ii+1));
-legend("25","30","35","40","50","60","70","80","90","100")
-hold on
+ for ii=1:2:19
+ loglog(T3(:,ii),T3(:,ii+1));
+ hold on
+ end
+ legend("25","30","35","40","50","60","70","80","90","100")
 
+
+for i=1:length(tires_edit_HFW.A_inch_2_)
+    A(i) = ((tires_edit_HFW.A_inch_2_(i)*n_rad)/L);% Hier eigentlich L^2 aber dann ergebnisse unrealistisch ; %Kontaktfläche aus Excel
+
+A(i) = A(i) *100;
+
+X=roundtowardvec(A(i),[5 8 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 110 120 130 140 150 160 170 180 190 200]);
+
+% rounding = (round(A(i)*200)/200)*100;
+% rounding = round(rounding);
+% 
+% % Wenn Wert lost zwischen zwei möglichen 
+% if rounding <=6 
+%    rounding =5;
+% elseif (rounding >=6 && rounding <=7)
+%     rounding = 8;
+% elseif rounding ==9
+%     rounding =10;
+% end
+
+
+
+[i2,j2]=find(T2(1,:)==X);
+if isempty(i2)
+    ewsl(i) = 0;
+    continue
 end
-
-
-for i=1:length(tires.A_inch_2_)
-    %z2 = zeros(1,1);
-    A(i) = (tires.A_inch_2_(i)*n_rad)/(L^2); %Kontaktfläche aus Excel
-
-
-rounding = (round(A(i)*5)/5);
-if rounding > 100
-    break
-end
-
-[i2,j2]=find(T2(1,:)==rounding);
-
 %figure(2);
 %plot(T2(:,j2),T2(:,j2+1));
 %hold on
-T2(:,(1:46)) = 0;
+
 z2(:,1) = T2(:,j2);
 z2(:,2) = T2(:,j2+1);
 z2(1,1) = 0;
@@ -172,25 +148,109 @@ z2(1,2) = 0;
 z2(2,1) = 0;
 z2(2,2) = 0;
 
-
 punkt_y= [P(2),P(2)];
 punkt_x = [0,6];
 punkt2 = [0 60;P(2) P(2)];
-%plot(punkt_x,punkt_y)
+plot(punkt_x,punkt_y)
 
 P2 = InterX(z2.',punkt2);
-
+if isempty(P2)
+    ewsl(i) = 0;
+    continue
+end
 reduc_fac(i) = P2(1);
 ewsl(i) = (tot_load/reduc_fac(i))/1000;
 
 % 3. Schritt ESWL bestimmen
 % ESWL und Reifendruck nutzen um LCN zu ermitteln
-loglog(tires.RatedInflation_PSI_(i),ewsl(i),'bx')
 
-%legend(tires.RatedInflation_PSI_(i))
+%figure(3)
+%loglog(tires_edit_HFW.RatedInflation_PSI_(i),ewsl(i),'bx')
+
+in(i) = inpolygon(tires_edit_HFW.RatedInflation_PSI_(i),ewsl(i),[71 88 218 238],[62 77 25 35]);
+clear figure(2)
 end
-Legend= linspace(1,length(ewsl),length(ewsl));
-legendStrings = "N = " + string(Legend);
-%legend("25","30","35","40","50","60","70","80","90","100",legendStrings)
+%% Check ob Reifen im LCN Limit liegt -> Über Polygons
+big = 0;
+big_idx = 0;
+for u=1:length(in)
+    if ewsl(u) > big && in(u) ==1
+        big = ewsl(u);
+        big_idx = u;
+    end
+end
+
+%% 2. Bugfahrwerk ermitteln
+%% Belastungen
+%min stat. Belastung -> hintere Lage
+F_BFW_min = (G_to * ((l_BFW+l_HFW)-l_BFW_max))/(l_BFW + l_HFW);
+
+%Max stat. Belastung -> Vordere Lage
+F_BFW_max = (G_to * ((l_BFW + l_HFW)-l_BFW_min))/(l_BFW + l_HFW);
+
+%Max dyn. Bremslast -> Abhängig von durchmesser Reifen
+%Outisde Diameter MAX verwenden?
+d_reifen = tires_edit_HFW.OutsideDiameterMax(big_idx);
+F_BFW_dyn(i) = F_BFW_max + ((10*(delta_z + 0.5 * d_reifen)*G_to) / (32.2 * (l_BFW + l_HFW)));
+
+
+% Statische Belastung muss zwischen 6% und 20% des Abfluggewichts liegen
+% NIEMALS Über/unterschreiten -> OPtimaler ist 8-15
+
+if ((F_BFW_min >= 0.06*G_to) && (F_BFW_max <= 0.20*G_to))
+    disp("Within Limits 1");
+    if ((F_BFW_min >= 0.08*G_to) && (F_BFW_max <= 0.15*G_to))
+        disp("Within Limits 2");
+    else
+        disp("Außerhalb Limits 2");
+    end
+else
+    disp("Außerhalb Limits 1");
+end
+
+% Statische Belastung muss mit 7% Reserve erhöht werden 
+% Auch 25% Reserve falls GTO sich erhöht, Aber A.Gobbin -> Wenn Roskam
+% fertig reicht nur 7% Betrachtung !!Kristof fragen!!
+
+%F_BFW_min = F_BFW_min + F_BFW_min * 0.07; -> Minimal nicht wichtig?
+
+%F_BFW_max = F_BFW_max + F_BFW_max * 0.07;
+
+%F_BFW_dyn = F_BFW_dyn + F_BFW_dyn .* 0.07; 
+
+%weitere 25% können weggelassen werden weil Roskam/Torenbeek gemacht
+%wurde??
+S_Reserve = 0.25;
+S_FW = 1 + ((0.07 + S_Reserve)/100);
+
+n_reifen_BFW = 2;
+
+F_reifen_BFW_max = (F_BFW_max/n_reifen_BFW) * S_FW;
+F_reifen_BFW_dyn = (F_BFW_dyn./n_reifen_BFW) .* S_FW;
+F_reifen_BFW_dyn_reduc = F_reifen_BFW_dyn./1.5;
+
+if F_reifen_BFW_dyn_reduc > F_reifen_BFW_max
+    reifencrit = F_reifen_BFW_dyn_reduc;
+else
+    reifencrit = F_reifen_BFW_max;
+end
+
+[~,j3]=find(tires.RatedInflation_PSI_>= 200 & tires.RatedLoad_Lbs_>=reifencrit & tires.RatedSpeed_MPH_>=v_reifen_krit);
+
+tires.PartNumber(j3)
+
+%% Auswahl Bugfahrwerksreifen
+
+
+
+
+
+
+%% Reifendruck 
+% Bugfahrwerk High Pressure ab 160PSI/ Aber auch 220PSI
+% Hauptfahrwerk Low-Pressure
+
+
+
 
 

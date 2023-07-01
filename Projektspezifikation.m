4ec1e331ad4d58260b5b583cabca87e3383dacf8
Jumper Schlumpf <schlumpfjumper@gmail.com>
2023-07-01 13:02:07 +0200

    Auto stash before merge of "main" and "origin/main"

--- Projektspezifikation.m
+++ Projektspezifikation.m
@@ -50,7 +50,7 @@ specs.flight_level = 340;
 
 specs.bypass = 10;                                  % Bypassverhaeltnis 
 specs.n_TW = 2;      % Anzahl Triebwerke
-m_TW = 7549;   % Quelle: https://www.easa.europa.eu/sites/default/files/dfu/EASA%20E111%20TCDS%20issue%2012_Trent%20XWB.pdf
+specs.m_TW = 7549;   % Quelle: https://www.easa.europa.eu/sites/default/files/dfu/EASA%20E111%20TCDS%20issue%2012_Trent%20XWB.pdf
 specs.m_TW = m_TW * 0.10 + m_TW; % Masse eines TW in kg bsp terd XWB
 
 % Masze Triebwerk (in meter)
@@ -59,6 +59,8 @@ specs.lh_TW=2.1;
 specs.Dn_TW=3.9;
 specs.Dh_TW=3.17;
 specs.l_TW=8.4;    %Muss verändert werden falls der rest sich ändert
+
+specs.Drosselgrad=[1,0.9,0.8];  %Drosselgrad (0.9 für steigflug und 0.8 für cruise)
 
 % Landespezifikationen
 

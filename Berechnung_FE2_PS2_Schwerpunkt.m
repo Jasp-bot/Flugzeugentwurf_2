e16b4aec3a79c2156f550d47a53e73f4d828dc27
Leocodes <leondoehler@gmail.com>
2023-07-03 17:55:21 +0200

    Erste Kartoffel

--- Berechnung_FE2_PS2_Schwerpunkt.m
+++ Berechnung_FE2_PS2_Schwerpunkt.m
@@ -127,7 +127,7 @@ CG_Wing_Z=CG_Wing_Moment_Z/CG_Wing_MZ;
 
 %% Bestimmung von X_MAC
 
-Wing_MAC.xSP_MAC_lmue = 0.23; % siehe Übung: Wert zwischen 20% und 25%
+Wing_MAC.xSP_MAC_lmue = 0.21; % siehe Übung: Wert zwischen 20% und 25%
 Wing_MAC.xSP_MAC_FG = CG_Wing_X - (CG_Data_Wing.Fluegel(2)-NP.l_mue_ges);
 
 Wing_MAC.XMAC = CG_Rumpf_X + Wing_MAC.xSP_MAC_FG*(CG_Wing_M/CG_M) - Wing_MAC.xSP_MAC_lmue*(1+(CG_Wing_M/CG_M))*NP.l_mue_ges;
@@ -140,7 +140,34 @@ CG_Gesamt_x = (Rumpf_MAC.xSP_MAC_RG*CG_M + Wing_MAC.xSP_MAC_FG*CG_Wing_M)/(CG_M 
 
 %% Bestimmung Schwerpunkt Tank
 
-% Irgendwie stimmt der Tankplot aus FE1 nicht
+% Schwerpunkt Außentrapez
+CG_Fuel_X.Epsilon_Aussen = Tank.c_aussen*tan(Ergebnisse_Fluegel.phi_VK_max) + Tank.b1_A;
+CG_Fuel_X.Aussentrapez = (Tank.b1_I^2 - Tank.b1_A^2 + CG_Fuel_X.Epsilon_Aussen*(Tank.b1_I + 2*Tank.b1_A))/(3*(Tank.b1_A+Tank.b1_I));
+CG_Fuel_X.Flaeche_Aussentrapez = 0.5*(Tank.b1_I + Tank.b1_A)*Tank.c_aussen;
+
+% Schwerpunkt Innentrapez
+CG_Fuel_X.Epsilon_Innen = Tank.c_innen*tan(Ergebnisse_Fluegel.phi_VK_max) + Tank.b1_I;
+CG_Fuel_X.Innentrapez = (Tank.b1_R^2 - Tank.b1_I^2 + CG_Fuel_X.Epsilon_Innen*(Tank.b1_R + 2*Tank.b1_I))/(3*(Tank.b1_R+Tank.b1_I));
+CG_Fuel_X.Flaeche_Innentrapez = 0.5*(Tank.b1_I + Tank.b1_R)*Tank.c_innen;
+
+%Schwerpunkt Rumpfteil
+CG_Fuel_X.Rumpf = 0.5*Tank.b1_R;
+CG_Fuel_X.Flaeche_Rumpf = Tank.b1_R*Tank.c_rumpf;
+
+% Gesamtschwerpunkt Tank im Flügelkoordinatensystem
+CG_Fuel_X.DeltaTankWing = DT.l_i_R*0.15 + DT.s_R*tan(Ergebnisse_Fluegel.phi_VK_max);
+CG_Fuel_X.Aussentrapez_FG = CG_Fuel_X.Aussentrapez + Tank.c_innen*tan(Ergebnisse_Fluegel.phi_VK_max) + CG_Fuel_X.DeltaTankWing;
+CG_Fuel_X.Innentrapez_FG = CG_Fuel_X.Innentrapez + CG_Fuel_X.DeltaTankWing;
+CG_Fuel_X.Rumpf_FG = CG_Fuel_X.Rumpf + CG_Fuel_X.DeltaTankWing;
+
+CG_Fuel_X.CG_Tank_FG = (CG_Fuel_X.Flaeche_Aussentrapez*CG_Fuel_X.Aussentrapez_FG + CG_Fuel_X.Flaeche_Innentrapez*CG_Fuel_X.Innentrapez_FG + CG_Fuel_X.Flaeche_Rumpf*CG_Fuel_X.Rumpf_FG)/(CG_Fuel_X.Flaeche_Rumpf + CG_Fuel_X.Flaeche_Innentrapez + CG_Fuel_X.Flaeche_Aussentrapez);
+
+% Gesamtschwerpunkt Tank im MAC-Koordinatensystem
+CG_Fuel_X.Aussentrapez_MAC = CG_Fuel_X.Aussentrapez_FG - (CG_Data_Wing.Fluegel(2)-NP.l_mue_ges);
+CG_Fuel_X.Innentrapez_MAC = CG_Fuel_X.Innentrapez_FG - (CG_Data_Wing.Fluegel(2)-NP.l_mue_ges);
+CG_Fuel_X.Rumpf_MAC = CG_Fuel_X.Rumpf_FG - (CG_Data_Wing.Fluegel(2)-NP.l_mue_ges);
+
+CG_Fuel_X.CG_Tank_MAC = CG_Fuel_X.CG_Tank_FG - (CG_Data_Wing.Fluegel(2)-NP.l_mue_ges);
 
 %% Bestimmung Schwerpunkt Frachträume
 
@@ -148,13 +175,64 @@ CG_Gesamt_x = (Rumpf_MAC.xSP_MAC_RG*CG_M + Wing_MAC.xSP_MAC_FG*CG_Wing_M)/(CG_M 
 
 %% Beladung
 
+% FUEL
+% Treibstoffmasse
+Betankung.Masse_Fuel_Aussen = 2*Tank.V_OB_A*Tank.ka*Tank.kb*1000*Tank.rho_kerosin;
+Betankung.Masse_Fuel_Innen_Theoretisch = 2*Tank.V_OB_I*Tank.ka*Tank.kb*1000*Tank.rho_kerosin;
+Betankung.Masse_Fuel_Innen_Praktisch = Ergebnisse_Massen_FE2.M_F-Betankung.Masse_Fuel_Aussen;
+% Betankung Außentank
+Betankung.CG_BetankterAussentank = (CG_Gesamt_x*Ergebnisse_Massen_FE2.M_OE + CG_Fuel_X.Aussentrapez_MAC*Betankung.Masse_Fuel_Aussen)/(Ergebnisse_Massen_FE2.M_OE + Betankung.Masse_Fuel_Aussen);
+Betankung.P1 = [CG_Gesamt_x/NP.l_mue_ges; Ergebnisse_Massen_FE2.M_OE];
+Betankung.P2 = [Betankung.CG_BetankterAussentank/NP.l_mue_ges; Ergebnisse_Massen_FE2.M_OE + Betankung.Masse_Fuel_Aussen];
+% Betankung Innentank
+Betankung.CG_BetankterInnentank = (Betankung.CG_BetankterAussentank*(Ergebnisse_Massen_FE2.M_OE + Betankung.Masse_Fuel_Aussen) + CG_Fuel_X.Innentrapez_MAC*Betankung.Masse_Fuel_Innen_Praktisch)/(Betankung.Masse_Fuel_Innen_Praktisch + Ergebnisse_Massen_FE2.M_OE + Betankung.Masse_Fuel_Aussen);
+Betankung.P3 = [Betankung.CG_BetankterInnentank/NP.l_mue_ges; (Betankung.Masse_Fuel_Innen_Praktisch + Ergebnisse_Massen_FE2.M_OE + Betankung.Masse_Fuel_Aussen)];
+
+
+% PAX Außen
+Pax_Bording = readtable("Bording.xlsx","Sheet","Beladung");
+AussenPax = table2array(Pax_Bording(1:49,"x"));
+AussenPaxMAC = AussenPax - Wing_MAC.XMAC;
+MasseAussenPax = table2array(Pax_Bording(1:49,"m"));
+% Front to Back
+CG_Shift_Outer = zeros(length(AussenPaxMAC),1);
+NewMassCounter = zeros(length(AussenPaxMAC),1);
+Mass_Shift_Outer = Ergebnisse_Massen_FE2.M_OE + Ergebnisse_Massen_FE2.M_F;
+CG_Startposition = Betankung.CG_BetankterInnentank;
+for i = 1:length(AussenPaxMAC)
+    newCGvalue = (CG_Startposition*Mass_Shift_Outer + AussenPaxMAC(i)*MasseAussenPax(i))/(Mass_Shift_Outer + MasseAussenPax(i)); % Hier können Sie den neuen Wert je nach Bedarf berechnen
+    CG_Shift_Outer(i) = newCGvalue; % Wert an der entsprechenden Stelle im Vektor zuweisen
+    NewMassCounter(i) = Mass_Shift_Outer + MasseAussenPax(i);
+    CG_Startposition = newCGvalue;
+    Mass_Shift_Outer = Mass_Shift_Outer + MasseAussenPax(i);
+end
+% Back to Front
+BackwardsAussenPaxMAC = flipud(AussenPaxMAC);
+BackwardsMasseAussenPax = flipud(MasseAussenPax);
+BackwardsCG_Shift_Outer = zeros(length(AussenPaxMAC),1);
+BackwardsNewMassCounter = zeros(length(AussenPaxMAC),1);
+BackwardsMass_Shift_Outer = Ergebnisse_Massen_FE2.M_OE + Ergebnisse_Massen_FE2.M_F;
+BackwardsCG_Startposition = Betankung.CG_BetankterInnentank;
+for i = 1:length(BackwardsAussenPaxMAC)
+    BackwardsnewCGvalue = (BackwardsCG_Startposition*BackwardsMass_Shift_Outer + BackwardsAussenPaxMAC(i)*BackwardsMasseAussenPax(i))/(BackwardsMass_Shift_Outer + BackwardsMasseAussenPax(i)); % Hier können Sie den neuen Wert je nach Bedarf berechnen
+    BackwardsCG_Shift_Outer(i) = BackwardsnewCGvalue; % Wert an der entsprechenden Stelle im Vektor zuweisen
+    BackwardsNewMassCounter(i) = BackwardsMass_Shift_Outer + BackwardsMasseAussenPax(i);
+    BackwardsCG_Startposition = BackwardsnewCGvalue;
+    BackwardsMass_Shift_Outer = BackwardsMass_Shift_Outer + BackwardsMasseAussenPax(i);
+end
+
+%% Plotten
+
 figure(1)
 hold on 
 grid on
 xlim([0 60])
 ylim([Ergebnisse_Massen_FE2.M_OE Ergebnisse_Massen_FE2.M_TO+10000])
 
-plot(40, 160000,'b-')
+plot([Betankung.P1(1)*100 Betankung.P2(1)*100], [Betankung.P1(2) Betankung.P2(2)],"b-")
+plot([Betankung.P2(1)*100 Betankung.P3(1)*100], [Betankung.P2(2) Betankung.P3(2)],"bo-")
+plot(CG_Shift_Outer*100/NP.l_mue_ges, NewMassCounter,"rx-")
+plot(BackwardsCG_Shift_Outer*100/NP.l_mue_ges, BackwardsNewMassCounter,"mx-")
 
 title('Beladung 3-Klassenbestuhlung','FontSize',20);
 xlabel('X^{MAC}_{SP}/l_{\mu}','FontSize',16)

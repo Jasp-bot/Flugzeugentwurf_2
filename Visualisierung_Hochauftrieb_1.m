53f8eccab175fb37efd51bb572f4a692732ecdbe
Maciej Janowski <mac.janowski@gmx.de>
2023-07-23 20:40:05 +0200

    teil 2

--- Visualisierung_Hochauftrieb_1.m
+++ Visualisierung_Hochauftrieb_1.m
@@ -24,13 +24,13 @@ plot(P3,P4,'black')
 plot(HA1.alpha_Ca_F_max, 0, 'xred')
 
 %CA MAX
-plot([0 20],[HA1.CA_F_max HA1.CA_F_max], 'red')
+plot([0 20],[HA1.CA_F_max HA1.CA_F_max], 'red--')
 plot(0, HA1.CA_F_max,"xred")
 
 %Alpha 0
 plot(HA1.alpha_MAC_0, 0,'xred')
 
-plot([HA1.alpha_Ca_F_max  HA1.alpha_Ca_F_max-HA1.delta_alpha_CA_F_max],[HA1.CA_F_max HA1.CA_F_max],'xgreen')
+plot([HA1.alpha_Ca_F_max  HA1.alpha_Ca_F_max-HA1.delta_alpha_CA_F_max],[HA1.CA_F_max HA1.CA_F_max],'xblue')
 
 grid on
 
@@ -42,7 +42,7 @@ h = HA1.alpha_Ca_F_max; % x-Koordinate des Maximums
 k = HA1.CA_F_max; % y-Koordinate des Maximums
 
 % Bereich der x-Achse
-x = linspace(h-7, h+7, 10); % Hier können Sie den Bereich anpassen
+x = linspace(h-5.5, h+7, 10); % Hier können Sie den Bereich anpassen
 
 % Quadratische Funktion berechnen
 y = -a * (x - h).^2 + k;
@@ -50,6 +50,6 @@ y = -a * (x - h).^2 + k;
 % Plot erstellen
 plot(x, y,"blue--",'LineWidth',1.5)
 plot(h, k, 'bx','LineWidth',1.5)
-ylim([-0.2, 2.1])
+ylim([-0.2, 1.6])
 xlim([-7, 20])
 
